1be7df458f7b62f3f9a0694dd87e318db36c64b8
JuananGarcia <55695749+JuananGarcia@users.noreply.github.com>
2024-08-23 00:50:20 +0200

    Update
    Not partial charge date from API.

--- lib/providers/football_provider.dart
+++ lib/providers/football_provider.dart
@@ -1,29 +1,17 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'dart:async';
 import '../models/match.dart';
-
 
 class FootballProvider with ChangeNotifier {
   List<Match> _matches = [];
   bool _loading = true;
 
-  FootballProvider() {
-    _startAutoRefresh();
-  }
-
   List<Match> get matches => _matches;
   bool get loading => _loading;
 
-  void _startAutoRefresh() {
-    Timer.periodic(Duration(seconds: 30), (timer) {
-      fetchMatches();
-    });
-  }
-
   Future<void> fetchMatches() async {
-    final url = Uri.parse('https://v3.football.api-sports.io/fixtures?date=2024-08-22');
+    final url = Uri.parse('https://v3.football.api-sports.io/fixtures?date=${DateTime.now()}');
     final response = await http.get(url, headers: {
       'x-rapidapi-host': 'v3.football.api-sports.io',
       'x-rapidapi-key': '28bd60988eadee8c34cfb7117f2ab229',
