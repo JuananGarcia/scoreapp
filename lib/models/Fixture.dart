fe840d82e28eebd38871bd74038d527ccc11d68f
JuananGarcia <55695749+JuananGarcia@users.noreply.github.com>
2024-08-23 16:07:52 +0200

    Revert "Update nulos"
    This reverts commit b1b4488d84a2c132ccb550a0a7d4645d73da90a8.

--- lib/models/Fixture.dart
+++ lib/models/Fixture.dart
@@ -19,13 +19,13 @@ class Fixture {
 
   factory Fixture.fromJson(Map<String, dynamic> json) {
     return Fixture(
-      id: json['id'] ?? 'nulo',
+      id: json['id'],
       referee: json['referee'] ?? 'Unknown Referee',
-      timezone: json['timezone'] ?? 'nulo',
+      timezone: json['timezone'],
       date: DateTime.parse(json['date']),
-      status: json['status']['long'] ?? 'nulo',
-      venue: json['venue']['name'] ?? 'nulo',
-      city: json['venue']['city'] ?? 'nulo',
+      status: json['status']['long'],
+      venue: json['venue']['name'],
+      city: json['venue']['city'],
     );
   }
 }
